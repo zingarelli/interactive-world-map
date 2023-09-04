f2bb18b5d7b3ffb76379fb8e4e9e4328bbcdab0a
Matheus Ricardo Uihara Zingarelli <zingarelli.m@gmail.com>
2023-09-04 19:02:57 -0300

    draw graticules

--- src/WorldMap/Marks/index.jsx
+++ src/WorldMap/Marks/index.jsx
@@ -1,8 +1,11 @@
-import { geoEqualEarth, geoPath } from "d3";
+import { geoEqualEarth, geoGraticule, geoPath } from "d3";
 
 // geoPath() returns a path generator function, which generates SVG path data for a given GeoJSON feature object. You can pass a map projection as parameter, which is a type of cartographic representation of the map. D3 already provides several projections. See more at https://github.com/d3/d3-geo-projection
 const projection = geoEqualEarth();
 const pathGenerator = geoPath(projection);
+
+// geoGraticule() is a function generator for creating gradicules (a grid of meridians and parallels), that can be used to draw meridian and parallel lines on the map
+const graticules = geoGraticule();
 
 // draw paths to create the geometry of each country of the world
 export default function Marks({ topology, population }) {
@@ -11,6 +14,11 @@ export default function Marks({ topology, population }) {
             {/* // path creates a shape, based on path commands passed to the d property. See the index.css for properties that can be used to style stroke color and other stuff) */}
             {/* { type: Sphere } is a supported object for path generator that will render the outline of the globe */}
             <path className="worldMap__globe" d={pathGenerator({ type: 'Sphere'})} />
+            <path
+                className="worldMap__graticules"
+                // graticules returns an object of type "MultiLineString", which is compatible with the path generator
+                d={pathGenerator(graticules())}
+            />
             {/* "features" property is a list of countries and the geometry for each of them (a set of [latitude, longitude] coordinates) */}
             {topology.features.map((feature, index) => {                                
                 const countryName = feature.properties.name;
